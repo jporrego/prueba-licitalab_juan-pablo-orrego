3bf24ed5e9f3cbc0345c4d118e9d36e3a9df6c3e
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-08-14 17:12:04 -0400

    date esLocale formatting

--- src/features/tasks/Task.tsx
+++ src/features/tasks/Task.tsx
@@ -9,6 +9,7 @@ import {
 import { Task as TaskType } from "../../types";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 import styles from "./Tasks.module.css";
+import { es as esLocale } from "date-fns/locale";
 
 interface TaskProps {
   task: TaskType;
@@ -20,8 +21,14 @@ const Task: React.FC<TaskProps> = ({ task }) => {
       <input type="checkbox" />
       <div className={styles.description}>{task.description}</div>
       <div className={styles.dates}>
-        <div>Created {formatDistanceToNow(task.creationDate)} ago</div>
-        <input type="Date"></input>
+        <div>
+          {`Tarea creada hace ${formatDistanceToNow(task.creationDate, {
+            locale: esLocale,
+          })}`}
+        </div>
+        <div>
+          Due date: <input type="Date"></input>
+        </div>
       </div>
       <BsFillCheckCircleFill></BsFillCheckCircleFill>
     </div>
