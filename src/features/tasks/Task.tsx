94ddd9edbc83aa34290c8103bf189b7615866d7a
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-08-15 15:59:52 -0400

    dueDate formatting

--- src/features/tasks/Task.tsx
+++ src/features/tasks/Task.tsx
@@ -1,11 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  format,
-  formatDistance,
-  formatRelative,
-  subDays,
-  formatDistanceToNow,
-} from "date-fns";
+import { format, formatDistanceToNow } from "date-fns";
 import { Task as TaskType } from "../../types";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 import styles from "./Tasks.module.css";
@@ -24,29 +18,31 @@ const Task: React.FC<TaskProps> = ({ task }) => {
   }, []);
 
   const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e);
+    console.log(new Date(e.target.value));
   };
 
   return (
     <div className={styles.task}>
       <input type="checkbox" />
       <div className={styles.description}>{task.description}</div>
+
       <div className={styles.dates}>
         <div>
           {`Tarea creada hace ${formatDistanceToNow(task.creationDate, {
             locale: esLocale,
           })}`}
         </div>
-        <div>
-          Due date:
+
+        <div className={styles.dueDate}>
+          <div>Vencimiento: </div>
           <input
             type="date"
             value={dueDate}
             onChange={(e) => handleDateChange(e)}
           ></input>
         </div>
-        <div>{format(task.dueDate, "dd/MM/yyyy")}</div>
       </div>
+
       <BsFillCheckCircleFill></BsFillCheckCircleFill>
     </div>
   );
